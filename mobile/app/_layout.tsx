e4e0bf590f59eab7bff3b2e1e04914992e228134
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2026-03-29 22:28:04 +0100

    Ajout de la prise en charge de PostHog pour l'analyse et mise à jour des dépendances dans package.json et .gitignore

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,14 +1,20 @@
 import { Stack } from "expo-router";
+import { PostHogProvider } from 'posthog-react-native';
 import '@/global.css';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{headerShown:false}}>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(annonces)" options={{ headerShown: false }} />
-      <Stack.Screen name="(points-relais)" options={{ headerShown: false }} />
-      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
-    </Stack>
+    <PostHogProvider
+      apiKey={process.env.EXPO_PUBLIC_POSTHOG_API_KEY!}
+      options={{ host: process.env.EXPO_PUBLIC_POSTHOG_HOST }}
+    >
+      <Stack screenOptions={{headerShown:false}}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(annonces)" options={{ headerShown: false }} />
+        <Stack.Screen name="(points-relais)" options={{ headerShown: false }} />
+        <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+      </Stack>
+    </PostHogProvider>
   );
 }
